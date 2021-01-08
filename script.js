d42856444fcabbaae6948561be4e5ad8c2f78143
zanonineto <zanoni.neto@hbsis.com.br>
2021-01-08 14:42:50 -0300

    2.3

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-$.ajax ({
-    url: 'https://hplussport.com/api/products',
-    success: function(response) {
-        console.log(response)
-    }
-})
+fetch('https://hplussport.com/api/products')
+    .then(
+        function (response) {
+            return response.json();
+        }
+    )
+    .then(
+        function (respData) {
+            console.log(respData);
+        }
+    )
