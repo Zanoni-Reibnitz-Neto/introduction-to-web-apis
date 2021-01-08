5390098016b98d01e3702da058d10ba09fcdd020
zanonineto <zanoni.neto@hbsis.com.br>
2021-01-08 15:34:46 -0300

    4.1

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 const request = new XMLHttpRequest();
 
-request.open('GET', 'https://hplussport.com/api/products?qty=2&order=name');
+request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=ZsEyWb6yPClrya3ZgDUrQV1CNqfrpmAd');
 
 request.onload = function() {
     const parsedData = JSON.parse(request.response);
     console.log(parsedData);
-
-    for(let item in parsedData) {
-        //Display all the product names
-        const products = document.createElement('li');
-        products.innerHTML = parsedData[item].name;
-        document.body.appendChild(products);
-
-        //Display all the product images
-        const images = document.createElement('img');
-        images.setAttribute('src', parsedData[item].image);
-        document.body.appendChild(images);
-    }
 }
 
 request.send();
