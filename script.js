e0707783a11801e270c57d8c8572f60d66539761
zanonineto <zanoni.neto@hbsis.com.br>
2021-01-08 14:27:08 -0300

    1.5

--- script.js
+++ script.js
@@ -2,13 +2,8 @@ const request = new XMLHttpRequest();
 
 request.open('GET', 'https://hplussport.com/api/products');
 
-request.onload = function () {
-    const response = request.response;
-    console.log(response);
-    console.log(typeof response);
-    const jsonData = JSON.parse(response);
-    console.log(jsonData);
-    console.log(typeof jsonData);
+request.onload = function() {
+    console.log(JSON.parse(request.response));
 };
 
 request.send();
