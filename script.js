a3547b14180dd5745abbe2b74b501344482b1d5f
zanonineto <zanoni.neto@hbsis.com.br>
2021-01-08 16:46:40 -0300

    4.3

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const request = new XMLHttpRequest();
 
-request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=ZsEyWb6yPClrya3ZgDUrQV1CNqfrpmAd');
+request.open('GET', 'https://api.giphy.com/v1/gifs/random?' +
+    'tag=squirrel&' +
+    'rating=g&' +
+    'api_key=ZsEyWb6yPClrya3ZgDUrQV1CNqfrpmAd'
+);
 
 request.onload = function() {
     const parsedData = JSON.parse(request.response);
