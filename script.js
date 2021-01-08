e771e35ea5e178723440f7a77c323019d29d8560
zanonineto <zanoni.neto@hbsis.com.br>
2021-01-08 15:28:29 -0300

    3.6

--- script.js
+++ script.js
@@ -5,9 +5,18 @@ request.open('GET', 'https://hplussport.com/api/products?qty=2&order=name');
 request.onload = function() {
     const parsedData = JSON.parse(request.response);
     console.log(parsedData);
-    const products = document.createElement('li');
-    products.innerHTML = parsedData[0].name;
-    document.body.appendChild(products);
+
+    for(item in parsedData) {
+        //Display all the product names
+        const products = document.createElement('li');
+        products.innerHTML = parsedData[item].name;
+        document.body.appendChild(products);
+
+        //Display all the product images
+        const images = document.createElement('img');
+        images.setAttribute('src', parsedData[item].image);
+        document.body.appendChild(images);
+    }
 }
 
 request.send();
